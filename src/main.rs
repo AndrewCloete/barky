e2e2434f84bd97c5d5b1e54c2892dc639eda3745
Andrew Cloete <andrewhcloete@gmail.com>
2023-06-25 10:37:03 +0200

    Basic microphone read using cpal

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
+
+#[derive(Parser, Debug)]
+#[command(version, about = "CPAL feedback example", long_about = None)]
+struct Opt {
+    /// The input audio device to use
+    #[arg(short, long, value_name = "IN", default_value_t = String::from("default"))]
+    input_device: String,
+
+    /// The output audio device to use
+    #[arg(short, long, value_name = "OUT", default_value_t = String::from("default"))]
+    output_device: String,
+
+    /// Specify the delay between input and output
+    #[arg(short, long, value_name = "DELAY_MS", default_value_t = 150.0)]
+    latency: f32,
+}
+
+fn main() -> anyhow::Result<()> {
+    let opt = Opt::parse();
+
+    #[cfg(any(
+        not(any(
+            target_os = "linux",
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "netbsd"
+        )),
+        not(feature = "jack")
+    ))]
+    let host = cpal::default_host();
+
+    // Find devices.
+    let input_device = if opt.input_device == "default" {
+        host.default_input_device()
+    } else {
+        host.input_devices()?
+            .find(|x| x.name().map(|y| y == opt.input_device).unwrap_or(false))
+    }
+    .expect("failed to find input device");
+
+    println!("Using input device: \"{}\"", input_device.name()?);
+
+    // We'll try and use the same configuration between streams to keep it simple.
+    let config: cpal::StreamConfig = input_device.default_input_config()?.into();
+
+    fn sample_to_string(sample: f32) -> String {
+        let count = ((sample + 1.0) * 20.0) as usize;
+        " ".repeat(count) + "o"
+    }
+
+    let input_data_fn = move |data: &[f32], _: &cpal::InputCallbackInfo| {
+        for &sample in data {
+            println!("{}", sample_to_string(sample))
+        }
+    };
+
+    // Build streams.
+    println!(
+        "Attempting to build both streams with f32 samples and `{:?}`.",
+        config
+    );
+    let input_stream = input_device.build_input_stream(&config, input_data_fn, err_fn, None)?;
+    println!("Successfully built streams.");
+
+    // Play the streams.
+    println!(
+        "Starting the input and output streams with `{}` milliseconds of latency.",
+        opt.latency
+    );
+    input_stream.play()?;
+
+    // Run for 3 seconds before closing.
+    println!("Playing for 3 seconds... ");
+    std::thread::sleep(std::time::Duration::from_secs(10));
+    drop(input_stream);
+    println!("Done!");
+    Ok(())
+}
+
+fn err_fn(err: cpal::StreamError) {
+    eprintln!("an error occurred on stream: {}", err);
 }
